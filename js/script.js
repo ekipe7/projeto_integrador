643517d5e2b185981ada2c807f21eca297d78b81
rafa-buenos <rafaelb1996@gmail.com>
2023-01-24 17:19:42 -0300

    Mudança do menu com js

---
+++ js/script.js
@@ -0,0 +1,45 @@
+/* Capturando elementos */
+
+let menu = document.querySelector(".menu");
+let ul = document.querySelector(".menu ul");
+let itens = document.getElementsByTagName('a');
+
+/* Criando botão de fechar */
+let fechar = document.createElement("div");
+fechar.innerText = " ";
+
+/* Estilizando botão fechar */
+fechar.style.position = "relative";
+fechar.style.width = "50px";
+fechar.style.height = "50px";
+fechar.style.top = "8%";
+fechar.style.right = "-10px";
+fechar.style.borderRadius = "10px";
+fechar.style.backgroundImage = "url(img/fechar.png)";
+fechar.style.backgroundSize = "100% 100%";
+
+/* Eventos */
+
+// Mostra menu
+menu.addEventListener("click", function(){
+    // Adiciona botão de fechar à ul
+    ul.insertBefore(fechar, itens[1]);
+
+    // Mostra o menu
+    ul.style.animationName = "abrir";
+    ul.style.animationDuration = "2s";
+    ul.style.display = "block";
+});
+
+// Esconde menu
+fechar.addEventListener("click", function(e){
+    ul.style.animationName = "fechar";
+    ul.style.animationDuration = "2s";
+    setTimeout(function(){
+        // Tira o menu
+        ul.style.display = "";
+    }, 1100)
+
+    // Impede que o evento menu seja encadeado
+    e.stopImmediatePropagation();
+});
